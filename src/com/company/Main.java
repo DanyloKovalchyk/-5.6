cee7d186c5cc4fea74f225debdcf6ac7f84403ae
Ковальчик Данило <danylo.danylo202@gmail.com>
2022-03-21 19:40:20 +0200

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //task 1
+        int a = 45;
+        int b = 5;
+        int c = a / b;
+        int d = a % b;
+
+        System.out.println("task 1: "+c+"; "+d);
+
+
+
+
+        //task 2
+        int i = 28;
+        int f = 28 % 10;
+        int g = (i/10)%10;
+
+        System.out.println("task 2: "+(f+g));
+
+
+
+        //task 3
+        double h = 3.5;
+        double k = Math.round(h);
+
+        System.out.println("task 3: "+k);
+
+    }
+    }
+
+
